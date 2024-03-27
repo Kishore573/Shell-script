76d4c8bf95af034d00ce8cfbb283cd3ea052af14
Kishore573 <kishorekumar.mergu@gmail.com>
2024-03-27 14:56:42 +0530

    added the array script

---
+++ 06-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+NAME=(Kishore, Supriya, Sahasra, Sanvita)
+
+echo "The First Person is: $NAME[0]"
+echo "The Second Person is: $NAME[1]"
+echo "The Third Person is: $NAME[2]"
+echo "The Fourth Person is: $NAME[3]"
+echo "All Person's names are: $NAME[@]"
