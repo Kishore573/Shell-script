cf8ce664312782eca131d53c586bafda378876f0
Kishore573 <kishorekumar.mergu@gmail.com>
2024-03-27 14:47:51 +0530

    added another script

---
+++ 05-variables.sh
@@ -0,0 +1,10 @@
+# This is for read the inputs through command line
+#!/bin/bash
+echo "Please enter the Username"
+read -s $USERNAME
+echo "You are entered username is: $USERNAME"
+
+echo "Please enter the Password"
+read -s $PASSWORD
+echo "You are entered password is: $PASSWORD"
+
