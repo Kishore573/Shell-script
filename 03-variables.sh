c14cfb366900bb8d57c83124052795d935bfcfae
Kishore573 <kishorekumar.mergu@gmail.com>
2024-03-27 11:31:10 +0530

    added one more variable script

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+echo "Hello World, I am learning DevOps"
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2 Good Morning"
+echo "$PERSON2: Hi $PERSON1 Hello, Good Morning" and $PERSON1 How are you?
+echo "$PERSON1: Hi $PERSON2 Yeah! I am also good and $PERSSON2 What about you?"
