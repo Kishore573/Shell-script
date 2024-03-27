ea14a2d213e2d493449e9646595b27f412253bbc
Kishore573 <kishorekumar.mergu@gmail.com>
2024-03-27 10:49:15 +0530

    modified some changes

--- 01-helloworld.sh
+++ 01-helloworld.sh
@@ -1,8 +1,8 @@
 #!bin/bash
 echo "Hello World, I am learning DevOps"
 
-PERSON1 = Kishore
-PERSON2 = Kumar
+PERSON1=Kishore
+PERSON2=Kumar
 
 echo "$PERSON1: Hi $PERSON2 Good Morning"
 echo "$PERSON2: Hi $PERSON1 Hello, Good Morning" and $PERSON1 How are you?
