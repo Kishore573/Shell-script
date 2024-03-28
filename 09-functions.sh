18d99753d4a881f7e354d31e99303cc80d240893
Kishore573 <kishorekumar.mergu@gmail.com>
2024-03-28 10:24:43 +0530

    added functions script

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
 USERID=$(id -u)
+DATE=$(date +F)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/&SCRIPT_NAME-$DATE.log 
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+VALIDATE () {
+
+if [ $1 -ne 0 ]
+then
+echo -e "The $2 .....$R FAILURE $N"
+exit 1
+else
+echo -e "The $2 ...... $E SUCCESS $N"
+fi 
+}
+
 
 if [ $USERID -ne 0 ]
 then
@@ -10,22 +29,11 @@ else
 echo "This is the root user"
 fi
 
-VALIDATE () {
-
-if [ $1 -ne 0 ]
-then
-echo "The $2 installation is not successfull"
-exit 1
-else
-echo "The $2 installation is successfull"
-fi 
-}
-
-yum install mysql -y
+yum install mysql -y &>>$LOGFILE
 
 VALIDATE $? "MYSQL installation is Success"
 
-yum install postfix -y
+yum install postfix -y &>>$LOGFILE
 
 VALIDATE $? "postfix installation is Success"
 
